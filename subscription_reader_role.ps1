d55d220e313e6537415f2653cec4b0a12c7c3da5
Rob McGuine <rmcguine@gmail.com>
2020-10-20 17:15:30 -0400

    Update subscription_reader_role.ps1

--- subscription_reader_role.ps1
+++ subscription_reader_role.ps1
@@ -4,12 +4,15 @@ $subs = Get-AzSubscription
 foreach ($sub in $subs) {
   $sub_scope = "/subscriptions/" + $sub.Id
   $assignedRoles = Get-AzRoleAssignment -Scope $sub_scope -SPN $appId
-    if ($assignedRoles.RoleDefinitionNamee.Contains('Reader')) {
-      write-host($sub_scope + " has Reader role enabled already")
-      }
-    else {
-      New-AzRoleAssignment -RoleDefinitionName "Reader" -ApplicationId $appId
-      write-host($sub_scope + " successfully added Reader role")
-      }
+  if (!$assignedRoles) {
+    New-AzRoleAssignment -Scope $sub_scope -RoleDefinitionName "Reader" -ApplicationId $appId
+    Write-Host($sub_scope + " successfully added Reader role")
+    }
+  elseif ($assignedRoles.RoleDefinitionName.Contains('Reader')) {
+    Write-Host($sub_scope + " has Reader role enabled already")
+    }
+  else {
+    New-AzRoleAssignment -Scope $sub_scope -RoleDefinitionName "Reader" -ApplicationId $appId
+    Write-Host($sub_scope + " successfully added Reader role")
+    }
  }
-      
