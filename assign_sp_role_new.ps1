f1338a90a9ff97f7eaa11b0430f207a403bdae60
Rob McGuine <rmcguine@gmail.com>
2020-10-21 17:30:11 -0400

    Create assign_sp_role_new.ps1

---
+++ assign_sp_role_new.ps1
@@ -0,0 +1,37 @@
+#Custom Role Script
+
+$role = Get-AzRoleDefinition -Name Contributor
+$role.Id = $null
+$role.Name = "CHTActions"
+$role.Description = "Grants access to Actions"
+$role.AssignableScopes.Clear()
+$role.Actions.Clear()
+$role.NotActions.Clear()
+$role.Actions.Add("Microsoft.Compute/*/write")
+$role.Actions.Add("Microsoft.Compute/*/action")
+$role.Actions.Add("Microsoft.ClassicCompute/*/write")
+$role.Actions.Add("Microsoft.ClassicCompute/*/action")
+$role.Actions.Add("Microsoft.Sql/*/write")
+$role.Actions.Add("Microsoft.Sql/*/action")
+$role.Actions.Add("Microsoft.Storage/*/write")
+$role.Actions.Add("Microsoft.Storage/*/action")
+$role.Actions.Add("Microsoft.ClassicStorage/*/write")
+$role.Actions.Add("Microsoft.ClassicStorage/*/action")
+$role.NotActions.Add("*/Delete")
+$role.NotActions.Add("Microsoft.Authorization/*")
+$subs = Get-AzSubscription
+foreach ($sub in $subs) {
+	$sub_scope = '/subscriptions/' + $sub.Id
+	$role.AssignableScopes.Add("$sub_scope")
+}
+New-AzRoleDefinition -Role $role
+
+#Assignment Script
+
+$subs = Get-AzSubscription
+$client_id = '<MANUALLY_ENTER_APPLICATION_CLIENT_ID>'
+$sp = Get-AzADServicePrincipal -SPN $client_id
+foreach ($sub in $subs) {
+	$sub_scope = '/subscriptions/' + $sub.Id
+	New-AzRoleAssignment -ObjectId $sp.Id -RoleDefinitionName CHTActions -Scope 	$sub_scope
+}
