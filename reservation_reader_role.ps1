936bdaaf9753f19a1a4ddaf8680e29330e5e689b
Rob McGuine <rmcguine@gmail.com>
2020-10-21 03:13:11 -0400

    Create reservation_reader_role.ps1

---
+++ reservation_reader_role.ps1
@@ -0,0 +1,21 @@
+# Download Az.Reservations package if you don't already have it -> https://www.powershellgallery.com/packages/Az.Reservations/0.9.0
+
+$app = Get-AzADApplication -DisplayName rmcguinetestaccount
+$appId = $app.ApplicationId
+$reservationOrders = Get-AzReservationOrder
+
+foreach ($reservation in $reservationOrders) {
+  $order_scope = $reservation.Id
+  $assignedRoles = Get-AzRoleAssignment -Scope $order_scope -SPN $appId
+  if (!$assignedRoles) {
+    New-AzRoleAssignment -Scope $order_scope -RoleDefinitionName "Reader" -ApplicationId $appId
+    Write-Host($order_scope + " successfully added Reader role")
+    }
+  elseif ($assignedRoles.RoleDefinitionName.Contains('Reader')) {
+    Write-Host($order_scope + " has Reader role enabled already")
+    }
+  else {
+    New-AzRoleAssignment -Scope $order_scope -RoleDefinitionName "Reader" -ApplicationId $appId
+    Write-Host($order_scope + " successfully added Reader role")
+    }
+ }
