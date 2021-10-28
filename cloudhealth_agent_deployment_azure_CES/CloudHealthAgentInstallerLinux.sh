db4abfcb8e2880c394cf4681ed2abb32bc9fccf9
Rob McGuine <rmcguine@gmail.com>
2021-10-28 13:28:28 -0400

    Update CloudHealthAgentInstallerLinux.sh

--- cloudhealth_agent_deployment_azure_CES/CloudHealthAgentInstallerLinux.sh
+++ cloudhealth_agent_deployment_azure_CES/CloudHealthAgentInstallerLinux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://s3.amazonaws.com/remote-collector/agent/v24/install_cht_perfmon.sh -O install_cht_perfmon.sh;
-sudo sh install_cht_perfmon.sh 24 9a2e0a85-e364-4c97-a1ad-1e34e7f644ce azure;
+sudo sh install_cht_perfmon.sh 24 <API_KEY> azure;
